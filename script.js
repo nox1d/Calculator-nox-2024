19615bb6242cb2ff1cce8c3d2d911fc9c5d78132
nox1d <114980767+nox1d@users.noreply.github.com>
2024-04-20 17:40:10 +0800

    Add math operation functions

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function operate() {
-    
+function operate(arr) {
+    let operator = arr[1];
+    let num1 = parseInt(arr[0]);
+    let num2 = parseInt(arr[2]);
+    let result;
+
+    if (operator == "+") result = sum(num1, num2);
+    if (operator == "-") result = subtract(num1, num2);
+    if (operator == "*") result = multiply(num1, num2);
+    if (operator == "/") result = divide(num1, num2);
+
+    console.log(result);
+}
+
+function sum(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a-b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a, b) {
+    return a/b;
 }
 
 function splitValues(values) {
@@ -17,7 +43,7 @@ let buttons = document.querySelectorAll("button");
 for (let button of buttons) {
     button.addEventListener("click", function() {
         if (this.className == "operator" && VALID_REGEX.test(values)) {
-            splitValues(values);
+            operate(splitValues(values));
         } else {
             SCREEN.innerText += this.innerText;
             values += (this.value);
