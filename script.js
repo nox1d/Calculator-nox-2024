2e96932cf8ff495cf0e2fdd1a32ae96a9a0dc0aa
nox1d <114980767+nox1d@users.noreply.github.com>
2024-04-20 18:31:02 +0800

    Fix calculator multiple operators logic

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function operate(arr) {
     if (operator == "*") result = multiply(num1, num2);
     if (operator == "/") result = divide(num1, num2);
 
-    console.log(result);
+    return result;
 }
 
 function sum(a, b) {
@@ -34,20 +34,29 @@ function splitValues(values) {
     return values_arr;
 }
 
-const VALID_REGEX = /^\d+[+\-*/]\d+$/;
+const VALID_REGEX = /^\d+[+\-x/]\d+$/;
+const OPERATOR_REGEX = /[+\-x/]$/;
 const SCREEN = document.getElementById("display");
-let values = "";
+
+let values = [];
 
 let buttons = document.querySelectorAll("button");
 
 for (let button of buttons) {
     button.addEventListener("click", function() {
-        if (this.className == "operator" && VALID_REGEX.test(values)) {
-            operate(splitValues(values));
+        if (this.className == "operator" && VALID_REGEX.test(SCREEN.innerText)) {
+            values = [operate(values)];
+            values.push(this.value)
+            console.log(values);
+            SCREEN.innerText = values[0] + this.innerText;
+        } else if (this.className == "operator" && OPERATOR_REGEX.test(SCREEN.innerText)) {
+            values[values.length - 1] = this.value;
+            console.log(values);
+            SCREEN.innerText = SCREEN.innerText.substring(0, SCREEN.innerText.length - 1) + this.innerText;
         } else {
+            values.push(this.value);
+            console.log(values);
             SCREEN.innerText += this.innerText;
-            values += (this.value);
-
         }
     });
 }
