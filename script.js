b41fdc38b302bc9e205df396dd94debd7991fa20
nox1d <114980767+nox1d@users.noreply.github.com>
2024-04-21 11:11:33 +0800

    Add dedicated functions for equal and operators

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// FUNCTIONS
+
 function operate(arr) {
     let operator = arr[1];
     let num1 = parseFloat(arr[0]);
@@ -10,6 +12,12 @@ function operate(arr) {
     if (operator == "/") result = divide(num1, num2);
 
     return result;
+}
+
+function splitValues(arr) {
+    let values_arr = arr.join("").replace(/^[*/]\d+/, 0).match(/^[+-]?(\d+([.]\d*)?|[.]\d+)|[+\-*/]|(\d+([.]\d*)?|[.]\d+)/g);
+    console.log(values_arr);
+    return values_arr;
 }
 
 function sum(a, b) {
@@ -28,11 +36,31 @@ function divide(a, b) {
     return a/b;
 }
 
-function splitValues(arr) {
-    let values_arr = arr.join("").replace(/^[*/]\d+/, 0).match(/^[+-]?(\d+([.]\d*)?|[.]\d+)|[+\-*/]|(\d+([.]\d*)?|[.]\d+)/g);
-    console.log(values_arr);
-    return values_arr;
+function getResult() {
+    if (OPERATOR_REGEX.test(SCREEN.innerText)) {
+        values.push(values[0]);
+        values = [operate(splitValues(values))];
+        SCREEN.innerText = values[0];
+    } else if (VALID_REGEX.test(SCREEN.innerText)) {
+        values = [operate(splitValues(values))];
+        SCREEN.innerText = values[0];
+    }
 }
+
+function continueOperation(button) { // make `2 + 3` into `6 [operator]` when you press another operator
+    values = [operate(splitValues(values))];
+    values.push(button.value);
+    console.log(values);
+    SCREEN.innerText = values[0] + button.innerText; 
+}
+
+function replaceOperator(button) {
+    values[values.length - 1] = button.value;
+    console.log(values);
+    SCREEN.innerText = SCREEN.innerText.substring(0, SCREEN.innerText.length - 1) + button.innerText;
+}
+
+// INSTANCES
 
 const VALID_REGEX = /^[+\-x/]?(\d+([.]\d*)?|[.]\d+)[+\-x/](\d+([.]\d*)?|[.]\d+)$/;
 const OPERATOR_REGEX = /[+\-x/]$/;
@@ -45,28 +73,16 @@ let buttons = document.querySelectorAll("button");
 for (let button of buttons) {
     button.addEventListener("click", function() {
         if (this.className == "equal") {
-            if (OPERATOR_REGEX.test(SCREEN.innerText)) {
-                values.push(values[0]);
-                values = [operate(splitValues(values))];
-                SCREEN.innerText = values[0];
-            } else if (VALID_REGEX.test(SCREEN.innerText)) {
-                values = [operate(splitValues(values))];
-                SCREEN.innerText = values[0];
-            }
+            getResult();
         } else if (this.className == "operator" && VALID_REGEX.test(SCREEN.innerText)) {
-            values = [operate(splitValues(values))];
-            values.push(this.value)
-            console.log(values);
-            SCREEN.innerText = values[0] + this.innerText;
+            continueOperation(this);
         } else if (this.className == "operator" && OPERATOR_REGEX.test(SCREEN.innerText)) {
-            values[values.length - 1] = this.value;
-            console.log(values);
-            SCREEN.innerText = SCREEN.innerText.substring(0, SCREEN.innerText.length - 1) + this.innerText;
+            replaceOperator(this);
         } else {
             values.push(this.value);
             console.log(values);
             SCREEN.innerText += this.innerText;
         }
-
+        
     });
 }
